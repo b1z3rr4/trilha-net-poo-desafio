0685746a0cdef25e8fa4d97c8120771bcf3f2c38
b1z3rr4 <nataliasilva.bezerra18@gmail.com>
2022-12-27 21:38:33 -0300

    chore: desafio completo

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,31 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) :
+            base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console
+                .WriteLine($"1 - Abrir Play Store e baixar {nomeApp} 2 - Voltar para Área de Trabalho");
+            string switch_on = Console.ReadLine();
+            switch (switch_on)
+            {
+                case "1":
+                    Console.WriteLine($"Baixando app {nomeApp}");
+                    break;
+                case "2":
+                    Console.WriteLine("Voltando para Área de Trabalho");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia celular1 = new Nokia("27996272063", "XLS", "123456789123", 4);
+celular1.InstalarAplicativo("freefire");
+
+Iphone celular2 = new Iphone("27996272063", "XLS", "123456789123", 4);
+celular2.InstalarAplicativo("freefire");
